ee86bb02245b6d5e40a1f096531a198c23397ad6
JuanJo Rey <juanreyfotos@gmail.com>
2021-06-28 11:16:32 +0200

    Spring validation

---
+++ src/main/java/com/example/form/controllers/FormController.java
@@ -0,0 +1,50 @@
+package com.example.form.controllers;
+
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.form.models.domain.Usuario;
+
+@Controller
+public class FormController {
+
+	@GetMapping("/form")
+	public String form(Model model) {
+		Usuario usuario = new Usuario();
+		model.addAttribute("titulo", "Formulario Usuario");
+		model.addAttribute("usuario", usuario);
+		return "form";
+	}
+
+	@PostMapping("/form")
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+
+		model.addAttribute("titulo", "Resultado form");
+
+		if (result.hasErrors()) {
+			
+			
+			
+//			Map<String, String> errores = new HashMap<>();
+//			result.getFieldErrors().forEach(err -> {
+//				errores.put(err.getField(),
+//						"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+//			});
+//			model.addAttribute("error", errores);
+
+			return "form";
+		}
+
+		model.addAttribute("usuario", usuario);
+
+		return "resultado";
+
+	}
+
+}
